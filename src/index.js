2894ef19b61ae8f2807d6137d369efd3a6be70ad
Martin Letellier <martinletellier17@gmail.com>
2025-04-23 10:56:12 +0200

    Mise à jour PWA: service worker + onUpdate

--- src/index.js
+++ src/index.js
@@ -6,6 +6,12 @@ import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
-serviceWorkerRegistration.register();
+// On enregistre le SW en passant un onUpdate
+serviceWorkerRegistration.register({
+    onUpdate: registration => {
+      // ici, on force le reload pour que l'utilisateur voit la nouvelle version
+      window.location.reload();
+    }
+  });
 
 
